deea51df663e7a4c9d9f1cd459ed1ac1d30220ad
Argha Paul <arghapaul77@gmail.com>
2024-11-11 23:24:32 +0530

    adding backend url

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -1,1 +1,5 @@
-export const BACKEND_URL = "http://localhost:8000";
+// export const BACKEND_URL = "http://localhost:8000";
+
+// export const BACKEND_URL = "/choreo-apis/pdfchatbot/backend/v1";
+
+export const BACKEND_URL = "https://23ff078d-7007-4ecc-a1ae-4f7846b8edee-dev.e1-us-east-azure.choreoapis.dev/pdfchatbot/backend/v1.0";
